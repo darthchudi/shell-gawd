59cf613ad708a804d7fce68670b4b42bdb30669d
chudi <chukwudioranu@ymail.com>
2018-08-22 20:49:48 +0100

    chore: remove unecessary file

--- day2/input-prompt/.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-read -p "Enter your username", username
-read -ps "Enter your password", password
-echo "Username: ${username}. Passsword: ${password} \n" >> credentials.txt
-echo "All Done! Thanks" 
